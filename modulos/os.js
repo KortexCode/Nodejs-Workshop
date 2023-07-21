923a77b24f98884b8c20d1eb5d16ca463b21fa6a
kortexcode <darkmaeno323@gmail.com>
2023-07-21 09:03:15 -0500

    Se agrega ejercicio en os.js

--- modulos/os.js
+++ modulos/os.js
@@ -1,3 +1,13 @@
+const os = require("os");
+
+console.log("Información del sistema:");
+
+const freeMemory = () => {
+  return os.freemem() / 1000000000;
+};
+
+console.table({ SystemOp: os.platform(), memoryGB: freeMemory() });
+
 // Actived Network interfaces right now
 console.log("Network Interfaces right now");
 console.log(os.networkInterfaces());
